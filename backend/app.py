dd95b5dd26066bf46aa6fb3776e08aaa932d3dc5
ehsan <ehsanhamzekhani80@gmail.com>
2025-01-05 18:27:58 +0330

    Dockerize the project

--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,23 @@ app = Flask(__name__)
 CORS(app)  # Enable cross-origin requests for the frontend
 
 
+@app.route("/")
+def home():
+    return "Flask backend is running!"
+
+
 @app.route("/api/chat", methods=["POST"])
 def chat():
     data = request.get_json()
     history = data.get("history", "")
     question = data.get("question", "")
     try:
+        # print test 
+        print("test ehsan")
         response = get_llm_response(history, question)
         return jsonify({"response": response})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}), 5001
 
 
 if __name__ == "__main__":
